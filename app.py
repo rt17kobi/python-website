35aca70d7df36d4403960f94e6851036b1c5447c
rohan tiwari <44979571+rt17kobi@users.noreply.github.com>
2023-03-19 20:19:17 +0000

    19.03.2023:21:17

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 
 # declaring the variable app which can inherit the Flask properties.
 app = Flask(__name__)
@@ -7,8 +7,9 @@ app = Flask(__name__)
 # @-> decorators
 @app.route("/")
 def hi():
-  return "Hi Rohan Tiwari"
+  return render_template('home.html')
 
 
+# to run the app on local host
 if __name__ == "__main__":
   app.run(host='0.0.0.0', debug=True)
